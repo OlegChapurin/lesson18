5ef654c9b5f5d0c512eb87344cb9898a4fc1b9c8
Oleg Chapurin <oleg_chapurin@mail.ru>
2019-12-25 02:03:18 +0400

    dz_8

---
+++ task0102/test/Gender.java
@@ -0,0 +1,8 @@
+package part01.lesson08.task0102.test;
+
+/**
+ * @author Oleg_Chapurin
+ */
+public enum Gender {
+    MAN, WOMAN;
+}
---
+++ task0102/test/TestAA.java
@@ -0,0 +1,15 @@
+package part01.lesson08.task0102.test;
+
+/**
+ * @author Oleg_Chapurin
+ */
+public class TestAA extends TestBB {
+    private static final Integer age = 50;
+    public boolean boo = true;
+    public int car = 10;
+    public char kkk = 56;
+    protected String str = "yyyyy";
+    public TestSS tes = new TestSS();
+    public float[] mm = {52,856,78};
+//    public ArrayList<TestSS> arrayList = new ArrayList<>();
+}
