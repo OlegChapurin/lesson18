a88f9ddd64bf96ed9186e02d2f212e6c182e561f
Oleg Chapurin <oleg_chapurin@mail.ru>
2019-12-25 01:52:54 +0400

    dz_5

---
+++ task01/AddValueException.java
@@ -0,0 +1,11 @@
+package part01.lesson05.task01;
+
+/**
+ *
+ * @author Oleg_Chapurin
+ */
+class AddValueException extends RuntimeException {
+    AddValueException(String exception) {
+        super(exception);
+    }
+}
---
+++ task01/Message.java
@@ -0,0 +1,22 @@
+package part01.lesson05.task01;
+
+/**
+ * Message list
+ *
+ * @author Oleg_Chapurin
+ */
+final class Message {
+    static final String ANIMAL = "Add  new animal (Yes) 1 or select from the list (Yes) 2, write list 3, exit enter 0.\n";
+    static final String ENTERNICKNAME = "Enter nickname\n";
+    static final String ENTERWEIGHT = "Enter weight\n";
+    static final String ENTERIDANIMAL = "Enter ID\n";
+    static final String EDITHOMEANIMAL = "Want to view data home animal (Yes) 1 or edit (Yes) 2, exit enter 0.\n";
+    static final String NEWWEIGHT = "Enter weight";
+    static final String ENTEROWNER = "Enter owner\n";
+    static final String NEWOWNER = "Create new owner (Yes) 1 or select from the list (Yes) 2, exit enter 0.\n";
+    static final String ENTERUIDOWNER = "Enter UID\n";
+    static final String GENDER = "Enter gender of owner (MAN) 1 (WOMAN) 0\n";
+    static final String AGE = "Enter age\n";
+    static final String NAME = "Enter name\n";
+    static final String CHANGE = "Change nickname (Yes) 1, weight (Yes) 2, owner (Yes) 3, exit enter 0.\n";
+}
